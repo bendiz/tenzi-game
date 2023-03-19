b2c242d224b46d381292f82a3bade002cc9ca2f4
Benedicte <101096042+bendiz@users.noreply.github.com>
2023-03-20 00:09:12 +0100

    Styling improvements to modal text

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,19 @@
 import { React, useState, useEffect } from "react";
 export default function Modal(props) {
   return (
-    <div className={`info-popup-backdrop`} onClick={props.closeModal}>
+    <div className={`info-popup-backdrop Modal`} onClick={props.closeModal}>
       <div className="tooltip">
         <span className="x" onClick={props.closeModal}>
           x
         </span>
+        <h2>🎲 Tenzi Rules</h2>
         <p>
           The objective of the game is to get all dice to the same number. Click
           each die to freeze it at its current value between rolls. It's very
           important to scream TENZI from the top of your lungs if you win.{" "}
           <br />
           <br />
-          Good luck and have fun!
+          <em>Good luck and have fun!</em>
         </p>
       </div>
     </div>
